d84799b75803b41721307fb94a0209ba82dba155
VPNam <vuphuongnam25122002@gmail.com>
2022-11-21 22:02:22 +0700

    [FEAT] Build login

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <title>Signin with Order Me !</title>
                     <link
                         href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap"
                         rel="stylesheet"
